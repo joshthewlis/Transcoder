e50c4766df5b4ce7711e911ebcd1fc8561366bf3
Josh Thewlis <josh@iancrt.co.uk>
2026-07-13 13:50:15 +0100

    Fix timeout for long I/O operations such as SMB movements

--- src/Transcoder.Worker/Program.cs
+++ src/Transcoder.Worker/Program.cs
@@ -4,7 +4,12 @@ using Transcoder.Worker.Services;
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.Configure<WorkerOptions>(builder.Configuration.GetSection("Transcoder"));
-builder.Services.AddHttpClient<WorkerApiClient>();
+builder.Services.AddHttpClient<WorkerApiClient>(client =>
+{
+    // Some server callbacks, especially job completion after large SMB/NAS copy operations,
+    // can legitimately take longer than HttpClient's default 100 second timeout.
+    client.Timeout = Timeout.InfiniteTimeSpan;
+});
 builder.Services.AddSingleton<PathMapper>();
 builder.Services.AddSingleton<CapabilityDetector>();
 builder.Services.AddSingleton<FfprobeRunner>();
